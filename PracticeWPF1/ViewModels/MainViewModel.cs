ad1b6ada64ededa339c0f62c40ec4c8a300d079d
Lee Dong Eon <dlehd9409@gmail.com>
2026-03-17 13:49:44 +0900

    버튼 클릭으로 화면의 색상 바꾸기 (현재 선택한 색상과 버튼의 색상이 일치하면 해당 버튼 막힘)

---
+++ PracticeWPF1/ViewModels/MainViewModel.cs
@@ -0,0 +1,54 @@
+﻿using PracticeWPF1.Helpers;
+using System.ComponentModel;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace PracticeWPF1.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private Brush _boxColor = Brushes.Gray; // 초기 색상
+
+        public Brush BoxColor
+        {
+            get => _boxColor;
+            set
+            {
+                _boxColor = value;
+                OnPropertyChanged(nameof(BoxColor));
+            }
+        }
+
+        public ICommand ChangeColorCommand { get; }
+
+        public MainViewModel()
+        {
+            // 첫 번째 인자: 실행 로직 (Execute)
+            // 두 번째 인자: 실행 가능 조건 (CanExecute)
+            ChangeColorCommand = new RelayCommand(
+                execute: colorName => {
+                    if (colorName is string name)
+                    {
+                        var brush = (Brush)new BrushConverter().ConvertFromString(name);
+                        BoxColor = brush;
+                    }
+                },
+                canExecute: colorName => {
+                    if (colorName is string name)
+                    {
+                        // 현재 색상과 버튼의 색상이 다를 때만 true 반환 (버튼 활성화)
+                        var targetBrush = (Brush)new BrushConverter().ConvertFromString(name);
+
+                        // Brush끼리의 비교는 ToString()이나 색상 코드 비교가 확실합니다.
+                        return BoxColor.ToString() != targetBrush.ToString();
+                    }
+                    return true;
+                }
+            );
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string name) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
---
+++ PracticeWPF1/Views/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using PracticeWPF1.ViewModels;
+using System.Windows;
+
+namespace PracticeWPF1.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = new MainViewModel();
+        }
+    }
+}
