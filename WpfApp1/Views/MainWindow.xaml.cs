3339b9647c9d2546f6638d98ccc1dba697d886e0
Lee Dong Eon <dlehd9409@gmail.com>
2026-03-17 13:55:25 +0900

    버튼 클릭으로 화면의 색상 바꾸기 (현재 선택한 색상과 버튼의 색상이 일치하면 해당 버튼 막힘)

---
+++ WpfApp1/Views/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using WpfApp1.ViewModels;
+using System.Windows;
+
+namespace WpfApp1.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = new MainViewModel();
+        }
+    }
+}
